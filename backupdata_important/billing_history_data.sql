a0d3fab1f89953ca34b41790e3e89867764b4e57
mhdalhzau <mhdalhzau@gmail.com>
2025-09-18 05:27:11 +0700

    Add files via upload

---
+++ backupdata_important/billing_history_data.sql
@@ -0,0 +1,30 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.9 (02a153c)
+-- Dumped by pg_dump version 16.9
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: billing_history; Type: TABLE DATA; Schema: public; Owner: neondb_owner
+--
+
+COPY public.billing_history (id, tenant_id, subscription_id, amount, currency, payment_method, status, paid_at, description, created_at) FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
